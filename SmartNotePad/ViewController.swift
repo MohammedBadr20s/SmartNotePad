85ee48497047b6dcb751438f456fb4e846c3cbe8
Mohammed Badr <Mohammed.badr20e@gmail.com>
2021-05-19 00:07:51 +0200

    Initial Commit

---
+++ SmartNotePad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartNotePad
+//
+//  Created by GoKu on 19/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
